2dcf026343be20ef51bc19d5d2e5a388ae2088b8
Derek Jokisch <djokisch@fmarion.edu>
2020-05-13 10:06:22 -0400

    plot2.R used to produced line plot2.png

---
+++ plot2.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(lubridate)
+## Reading in "household_power_consumption.txt"
+household <- read.table("household_power_consumption.txt", sep=";",
+                        stringsAsFactors = FALSE, header=TRUE, na.strings = "?")
+household$Time <- paste(household$Date,household$Time,sep=" ")
+household <- tbl_df(household)
+## household$Date <- as.Date(household$Date,format="%d/%m/%Y")
+## household$Time <- strptime(household$Time, format = "%d/%m/%Y %H:%M:%S")
+household$Date <- dmy(household$Date)
+household$Time <- dmy_hms(household$Time)
+
+## Filtering out all rows except for those on the two days desired.
+household <- filter(household, Date==as.Date("2007-02-01") |
+                        Date==as.Date("2007-02-02"))
+
+## Opening graphics device PNG
+png("plot2.png", width=480, height=480)
+
+## Producing plot
+plot(household$Time, household$Global_active_power,
+      type="l",
+      ylab="Global Active Power (kilowatts)",
+      xlab="",
+      pch=NA)
+
+## Closing the graphics device
+dev.off()
